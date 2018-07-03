676406f4e07b1717072b039c7df9b57855fb4ab9
zhou <fourierzhou@163.com>
2018-07-03 14:12:02 +0800

    上传分支fourier

--- artapp/models.py
+++ artapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ArtTag(models.Model):
+    title = models.CharField(max_length=20,
+                             verbose_name='作品类别',
+                             unique=True,
+                             db_index=True)
+
+    add_time = models.DateTimeField(verbose_name='添加时间',auto_now_add=True)
+    modify_time = models.DateTimeField(verbose_name='修改时间',auto_now=True)
